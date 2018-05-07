8c11984f1d8ed99fadd7c88d9d5eff5ac3dd84db
George Alves <gvictor00@gmail.com>
2018-05-07 12:22:41 -0300

    Codigo inicial

---
+++ exercicios/exPag194/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int soma = 0, i = 0, peso = 0;
+    int somaPesos = 0;
+    int somaPonderada = 0;
+    int vetor[10] = {0};
+    int normalizado[10] = {0};
+    int mult = 1;
+
+    int maior = 0, menor = 10000;
+
+    // Lê o vetor
+    for(i = 0; i < 10; i++)
+    {
+        printf("Digite o numero #%d: ", i+1);
+        scanf("%d",&vetor[i]);
+
+        if(vetor[i] < menor)
+            menor = vetor[i];
+
+        if(vetor[i] > maior)
+            maior = vetor[i];
+
+        // Realiza a soma
+        soma += vetor[i];
+
+        // Verifica se é uma posicao par e multiplica, caso seja.
+        if(i%2)
+        {
+            mult *= vetor[i];
+        }
+
+        // Realiza a soma ponderada
+        peso = i + 1;
+        somaPesos += peso;
+        somaPonderada += vetor[i] * peso;
+    }
+
+    printf("Soma: %d\n", soma);
+    printf("Multiplicacao: %d\n", mult);
+    printf("Media ponderada: %d\n", (somaPonderada/somaPesos));
+
+    printf("Vetor normalizado: ")
+    for(i = 0; i < 10; i++)
+    {
+        normalizado[i] = (vetor[i]-menor)/(maior - menor);
+        printf("%d ", normalizado[i]);
+    }
+    printf("\n");
+    return 0;
+}
