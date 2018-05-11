b9c1a736bcbdae61cbdd9bf82b90c3ffaae9164a
George <gvictor00@gmail.com>
2018-05-11 00:59:48 -0300

    Inclui o resto dos itens

--- projeto/drawMonsters/main.c
+++ projeto/drawMonsters/main.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "monsters.h"
+#include "deslocamento.h"
 
 int main()
 {
+    int sair = 0;
+    char tecla;
+    int ret = 0;
+
+    tecla = getch();
+    printf("%d",tecla);
+    while(sair != 1)
+    {
+        tecla = getch();
+        switch(tecla)
+        {
+        case 75:
+            moveEsquerda();
+            //printf("Seta para esquerda\n");
+            desenhaMatriz();
+            break;
+        case 72:
+            moveCima();
+            //printf("Seta para cima\n");
+            desenhaMatriz();
+            break;
+        case 77:
+            moveDireita();
+            //printf("Seta para direita\n");
+            desenhaMatriz();
+            break;
+        case 80:
+            moveBaixo();
+            //printf("Sezta para baixo\n");
+            desenhaMatriz();
+            break;
+        case 32:
+            printf("Atirou!\n");
+            break;
+        case 27:
+            printf("Sair\n");
+            sair = 1;
+            break;
+        }
+    }
+
     drawMonster(1);
     printf("\n");
     drawMonster(2);
