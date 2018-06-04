be408219b271e2743085e039f86b187f2f6b5048
George Alves <gvictor00@gmail.com>
2018-06-04 10:42:29 -0300

    Inclui arquivos iniciais

---
+++ projeto/spaceInvaders/invStructs.h
@@ -0,0 +1,11 @@
+#ifndef INVSTRUCTS_H_INCLUDED
+#define INVSTRUCTS_H_INCLUDED
+
+typedef struct player{
+    int linha;
+    int coluna;
+    int nome[20];
+    int pontuacao;
+}Player;
+
+#endif // INVSTRUCTS_H_INCLUDED
