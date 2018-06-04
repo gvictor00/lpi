90251a95678e85565e2d0d60aa76cfbcf59d69be
George Alves <gvictor00@gmail.com>
2018-06-04 10:24:09 -0300

    Inclui comentário com autor

--- projeto/spaceInvaders/main.c
+++ projeto/spaceInvaders/main.c
@@ -1,3 +1,11 @@
+/*!
+\brief      Projeto de LPI - Space Invaders
+\details    Projeto para desenvolvimento de um jogo Space Invaders em linguagem C.
+\author     George Alves
+\date       2018
+\warning    O uso indevido pode quebrar o programa!
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
